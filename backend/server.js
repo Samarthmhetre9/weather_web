1f3f5f2bdcf4fe2a41ef24201b183f25541fa6ef
Samarth Mhetre <sammhetre2512@gmail.com>
2026-05-12 09:40:52 +0530

    added root route

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send('API Running');
+});
+
 // MongoDB Connection
 mongoose.connect("mongodb://127.0.0.1:27017/weatherapp")
 .then(() => console.log("MongoDB Connected"))
